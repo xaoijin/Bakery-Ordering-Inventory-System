c938a849bc07d25d4763023e501fd8b7f16c14db
Johnson Li <liwindx15@gmail.com>
2022-11-17 23:42:22 -0500

    added the vectors needed and the scanner

--- BreadBoss/src/boss/Store.java
+++ BreadBoss/src/boss/Store.java
@@ -1,5 +1,19 @@
 package boss;
+
+import java.util.Scanner;
+import java.util.Vector;
+
 //this is where all the methods will be
 public class Store {
+	Vector <Products> item;
+	Vector <Users> account;
+	Vector <Orders> invoice;
+	Scanner Keyboard;
 	
+	Store(){
+		item = new Vector<Products>();
+		account = new Vector<Users>();
+		invoice = new Vector<Orders>();
+		Keyboard = new Scanner (System.in);
+	}
 }
