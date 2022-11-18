850de849d4fb83e79f0e0bc88adc6517a01390be
Johnson Li <liwindx15@gmail.com>
2022-11-17 21:25:16 -0500

    Finished Users Class Testing Discord Notif Signed-off-by: Johnson Li

--- BreadBoss/src/boss/Users.java
+++ BreadBoss/src/boss/Users.java
@@ -1,5 +1,64 @@
 package boss;
 
 public class Users {
-
+	private String userID;
+	private String username;
+	private String password;
+	private String Phone;
+	private String Email;
+	private String Name;
+	private boolean isEmployee;
+	public Users(String userID, String username, String password, String phone, String email, String name,
+			boolean isEmployee) {
+		super();
+		this.userID = userID;
+		this.username = username;
+		this.password = password;
+		Phone = phone;
+		Email = email;
+		Name = name;
+		this.isEmployee = isEmployee;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public boolean isEmployee() {
+		return isEmployee;
+	}
+	public void setEmployee(boolean isEmployee) {
+		this.isEmployee = isEmployee;
+	}
 }
