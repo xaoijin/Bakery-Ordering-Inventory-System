8cae82f55fb5a7d23e8afe2db8475cab5ed5344b
Johnson Li <liwindx15@gmail.com>
2022-12-02 22:24:27 -0500

    Updated products class with just description

--- BreadBoss/src/boss/Products.java
+++ BreadBoss/src/boss/Products.java
@@ -5,82 +5,43 @@ public class Products {
 	private String productID; 
 	private String productName;
 	private double price;
-	private boolean isVegan; //checksif ProductisVegan
-	private boolean isNutFree; //checks if Product is Nut Free
-	private boolean isDairyFree; //checks if Product is Dairy Free
-	
-	public Products(String productID, String productName, double price, boolean isVegan, boolean isNutFree,
-			boolean isDairyFree) 
-	{
+	private String description;
+	public Products(String productID, String productName, double price, String description) {
 		super();
 		this.productID = productID;
 		this.productName = productName;
 		this.price = price;
-		this.isVegan = isVegan;
-		this.isNutFree = isNutFree;
-		this.isDairyFree = isDairyFree;
+		this.description = description;
 	}
-	
-	public Products() 
-	{
-		this.productID ="NULL";
+	public Products() {
+		this.productID = "NULL";
 		this.productName = "NULL";
 		this.price = 0.00;
-		this.isVegan = false;
-		this.isNutFree = false;
-		this.isDairyFree = false;
+		this.description = "NULL";
 	}
-	
-	
-	public String getProductID() 
-	{
+	public String getProductID() {
 		return productID;
 	}
-	public void setProductID(String productID)
-	{
+	public void setProductID(String productID) {
 		this.productID = productID;
 	}
-	public String getProductName()
-	{
+	public String getProductName() {
 		return productName;
 	}
-	public void setProductName(String productName) 
-	{
+	public void setProductName(String productName) {
 		this.productName = productName;
 	}
-	public double getPrice() 
-	{
+	public double getPrice() {
 		return price;
 	}
-	public void setPrice(double price)
-	{
+	public void setPrice(double price) {
 		this.price = price;
 	}
-	public boolean isVegan() 
-	{
-		return isVegan;
+	public String getDescription() {
+		return description;
 	}
-	public void setVegan(boolean isVegan)
-	{
-		this.isVegan = isVegan;
+	public void setDescription(String description) {
+		this.description = description;
 	}
-	public boolean isNutFree() 
-	{
-		return isNutFree;
-	}
-	public void setNutFree(boolean isNutFree) 
-	{
-		this.isNutFree = isNutFree;
-	}
-	public boolean isDairyFree() {
-		return isDairyFree;
-	}
-	public void setDairyFree(boolean isDairyFree) 
-	{
-		this.isDairyFree = isDairyFree;
-	}
-	
-	
-	
 	
 }
